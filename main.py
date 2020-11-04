c6e4ccd65ad4cfe79ef9046fdd812fe9f5288492
DESKTOP-TBBDDJ1\Cyber <kolinszeto@gmail.com>
2020-11-04 15:52:47 -0800

    updated the code with comments about post

--- main.py
+++ main.py
@@ -22,19 +22,19 @@ def chessEmbed():
 def helloThere():
     #function use Flask import (Jinga) to render an HTML template
     return render_template("helloThere.html")
-
-@app.route("/yourName")
+#---------------------------------------------------------------------------------------
+@app.route("/yourName") #within the /yourName directory
 def index():
-    return render_template("yourName.html", display="")
+    return render_template("yourName.html", display="") #this is the default what is displayed (right now it is nothing)
 
-@app.route("/printName", methods=['GET','POST'])
+@app.route("/printName", methods=['GET','POST']) #this is is where the website directs to when clicking the submit button
 def yourName():
-    if request.method == 'POST':
+    if request.method == 'POST': #if the meathod is post
         form = request.form
-        returnName = str(form['inputName'])
-        return render_template("yourName.html", display = returnName)
-    return redirect("/yourName")
-
+        returnName = str(form['inputName']) #takes the input and converts it to a string, if it was a integer data type be sure to change str to int
+        return render_template("yourName.html", display = returnName) #feeds it back into the template using jinja
+    return redirect("/yourName")#redirects the user back to the page where they entered in the form
+#---------------------------------------------------------------------------------------
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True)
